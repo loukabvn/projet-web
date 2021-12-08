d82648a41fda6f82d523e2294d47c88a035f94e0
Louka <louka.bvn@gmail.com>
2021-12-08 14:03:44 +0100

    admin & maintainer DAO

--- src/main/java/DAO/AdminDAO.java
+++ src/main/java/DAO/AdminDAO.java
@@ -1,12 +1,11 @@
 package DAO;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.sql.ResultSet;
-import java.util.List;
 
 import DBUtils.Database;
-
 import model.Admin;
 
 public class AdminDAO {
@@ -19,39 +18,32 @@ public class AdminDAO {
 	
 	private static final String TABLE_NAME		= "Admin";
 	
-	private static final String ID_FIELD 		= "id";
-	private static final String USERNAME_FIELD	= "username";
-	private static final String EMAIL_FIELD 	= "email";
-	private static final String PASSWORD_FIELD	= "password";
+	private static final String ID_FIELD 		= "AdminId";
+	private static final String USERNAME_FIELD	= "AdminName";
+	private static final String EMAIL_FIELD 	= "AdminMail";
+	private static final String PASSWORD_FIELD	= "AdminPassword";
 	
 	public static Admin get(String email) {
 		checkEmail(email);
 		String query = "SELECT * FROM " + TABLE_NAME
 				+ " WHERE " + EMAIL_FIELD + " = " + email;
-		
 		Admin admin = null;
-		List<Integer> l;
 		try {
-			
 			ResultSet res = Database.execQuery(query);
-			res.next();
-			
-			admin = new Admin(res.getString(USERNAME_FIELD),
-					res.getString(EMAIL_FIELD), res.getString(PASSWORD_FIELD));
-			
-			admin.setID(res.getInt(ID_FIELD));
-			
-			l = MaintainerDAO.getList(admin.getID());
-			if (l == null) {
-				admin = null;
-			} else {
-				admin.setMaintainersList(l);
+			if (res.next()) {
+				admin = new Admin(res.getString(USERNAME_FIELD),
+						res.getString(EMAIL_FIELD), res.getString(PASSWORD_FIELD));
+				admin.setID(res.getInt(ID_FIELD));
+				List<Integer> list = MaintainerDAO.getList(admin.getID());
+				if (list == null) {
+					admin = null;
+				} else {
+					admin.setMaintainersList(list);
+				}
 			}
-			
-			
 		} catch (SQLException e) {
-			e.printStackTrace();
 			admin = null;
+			e.printStackTrace();
 		}
 		return admin;
 	}
@@ -62,20 +54,16 @@ public class AdminDAO {
 		}
 		String check = "SELECT * FROM " + TABLE_NAME
 				+ " WHERE " + EMAIL_FIELD + " = " + admin.getEmail();
-		
 		String query = "INSERT INTO " + TABLE_NAME + "("
 				+ USERNAME_FIELD + "," + EMAIL_FIELD + "," + PASSWORD_FIELD
 				+ ") VALUES(" + admin.getUsername() + ","
 				+ admin.getEmail() + "," + admin.getPassword() + ");";
 		try {
-			
 			ResultSet res = Database.execQuery(check);
 			if (res.next()) {
 				throw new AssertionError("This email is already in the database");
 			}
-			
 			Database.execUpdate(query);
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -85,7 +73,6 @@ public class AdminDAO {
 		if (admin == null) {
 			throw new AssertionError("Invalid parameters");
 		}
-		
 		String query = "UPDATE " + TABLE_NAME
 				+ " SET " + USERNAME_FIELD + " = " + admin.getUsername() + ","
 				+ EMAIL_FIELD + " = " + admin.getEmail() + ","
@@ -104,7 +91,6 @@ public class AdminDAO {
 		}
 		String query = "DELETE FROM " + TABLE_NAME
 				+ " WHERE " + ID_FIELD + " = " + admin.getID() + ";";
-		
 		try {
 			Database.execUpdate(query);
 		} catch (SQLException e) {
--- src/main/java/DAO/MaintainerDAO.java
+++ src/main/java/DAO/MaintainerDAO.java
@@ -1,28 +1,125 @@
 package DAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
+import DBUtils.Database;
 import model.Maintainer;
 
 public class MaintainerDAO {
+	
+	private static final String EMAIL_REGEX =
+			"^[a-zA-Z0-9_+&*-]+(?:\\."+
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,7}$";
+	
+	private static final String TABLE_NAME		= "Maintainer";
+	
+	private static final String ID_FIELD 		= "MainId";
+	private static final String USERNAME_FIELD	= "MainName";
+	private static final String EMAIL_FIELD 	= "MainMail";
+	private static final String PASSWORD_FIELD	= "MainPassword";
 
-	public static Maintainer get(String email)  {
-		return new Maintainer(null, null, null);
+	public static Maintainer get(String email) {
+		checkEmail(email);
+		String query = "SELECT * FROM " + TABLE_NAME
+				+ " WHERE " + EMAIL_FIELD + " = " + email;
+		Maintainer maintainer = null;
+		try {
+			ResultSet res = Database.execQuery(query);
+			if (res.next()) {
+				maintainer = new Maintainer(res.getString(USERNAME_FIELD),
+						res.getString(EMAIL_FIELD), res.getString(PASSWORD_FIELD));
+				maintainer.setID(res.getInt(ID_FIELD));
+				List<Integer> list = RessourceDAO.getList(maintainer.getID());
+				if (list == null) {
+					maintainer = null;
+				} else {
+					maintainer.setRessourcesList(list);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			maintainer = null;
+		}
+		return maintainer;
 	}
 	
-	public static List<Integer> getList(int AdminId) {
-		return null;
+	public static List<Integer> getList(int adminID) {
+		String query = "SELECT " + ID_FIELD + " FROM " + TABLE_NAME;
+		List<Integer> list = new ArrayList<Integer>();
+		try {
+			ResultSet res = Database.execQuery(query);
+			while (res.next()) {
+				list.add(res.getInt(ID_FIELD));
+			}
+		} catch (SQLException e) {
+			list = null;
+			e.printStackTrace();
+		}
+		return list;
 	}
 
-	public static int insert(Maintainer maintainer) {
-		return 0;
+	public static void insert(Maintainer maintainer) {
+		if (maintainer == null) {
+			throw new AssertionError("Invalid parameters");
+		}
+		String check = "SELECT * FROM " + TABLE_NAME
+				+ " WHERE " + EMAIL_FIELD + " = " + maintainer.getEmail();
+		String query = "INSERT INTO " + TABLE_NAME + "("
+				+ USERNAME_FIELD + "," + EMAIL_FIELD + "," + PASSWORD_FIELD
+				+ ") VALUES(" + maintainer.getUsername() + ","
+				+ maintainer.getEmail() + "," + maintainer.getPassword() + ");";
+		try {
+			ResultSet res = Database.execQuery(check);
+			if (res.next()) {
+				throw new AssertionError("This email is already in the database");
+			}
+			Database.execUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public static int update(Maintainer maintainer) {
-		return 0;
+	public static void update(Maintainer maintainer) {
+		if (maintainer == null) {
+			throw new AssertionError("Invalid parameters");
+		}
+		String query = "UPDATE " + TABLE_NAME
+				+ " SET " + USERNAME_FIELD + " = " + maintainer.getUsername() + ","
+				+ EMAIL_FIELD + " = " + maintainer.getEmail() + ","
+				+ PASSWORD_FIELD + " = " + maintainer.getPassword() + " "
+				+ "WHERE " + ID_FIELD + " = "+ maintainer.getID() + ";";
+		try {
+			Database.execUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public static int delete(Maintainer maintainer) {
-		return 0;
+	public static void delete(Maintainer maintainer) {
+		if (maintainer == null) {
+			throw new AssertionError("Invalid parameters");
+		}
+		String query = "DELETE FROM " + TABLE_NAME
+				+ " WHERE " + ID_FIELD + " = " + maintainer.getID() + ";";
+		try {
+			Database.execUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Private methods
+	
+	private static void checkEmail(String email) {
+		Pattern pat = Pattern.compile(EMAIL_REGEX);
+		if (email == null || !pat.matcher(email).matches()) {
+			throw new AssertionError("Invalid email !");
+		}
 	}
 }
