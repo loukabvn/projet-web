33b001ffa5c97e15244724786b1d4c5fd36156f3
Louka <louka.bvn@gmail.com>
2021-12-20 13:13:53 +0100

    install plus jolie

--- install.sh
+++ install.sh
@@ -5,34 +5,13 @@
 
 # colors
 
-#!/bin/bash
-
-# Ansi color code variables
 red="\e[0;91m"
 blue="\e[0;94m"
-expand_bg="\e[K"
-blue_bg="\e[0;104m${expand_bg}"
-red_bg="\e[0;101m${expand_bg}"
-green_bg="\e[0;102m${expand_bg}"
 green="\e[0;92m"
 white="\e[0;97m"
 bold="\e[1m"
 uline="\e[4m"
 reset="\e[0m"
-
-# colored text
-echo -e "${red}Hello World!${reset}"
-echo -e "${blue}Hello World!${reset}"
-echo -e "${green}Hello World!${reset}"
-echo -e "${white}Hello World!${reset}"
-
-echo ""
-
-# bold colored text
-echo -e "${red}${bold}Hello World!${reset}"
-echo -e "${blue}${bold}Hello World!${reset}"
-echo -e "${green}${bold}Hello World!${reset}"
-echo -e "${white}${bold}Hello World!${reset}"
 
 # useful functions
 
