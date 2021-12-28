a54e033e807f1a44aeaa5faf286c1d13d2edbab7
Louka <louka.bvn@gmail.com>
2021-12-28 19:55:51 +0100

    petites modifs

--- insert.sql
+++ insert.sql
@@ -4,8 +4,8 @@ INSERT INTO
     Maintainer(MainName, MainMail, MainPassword, MainSalt, MainStatus)
 VALUES
     -- Mot de passe : Password#1
-    ("Default", "default@domain.com", "H1Y2hZ91pue8S_aMVJe5ekJS-wWNP1aU3ccrM-z0R3Y=",
-        "ra_1i2hOcUaRQWXQLwVr34hBrsHzzgr2tC1hepbwWek=", 1),
+    ("Default1", "default1@domain.com", "zamMZxUqbTscWQTUG5W0ET60lna_tdHAUQ0SFvlXKlY=",
+        "kf3-Re6E00xc8rnnRN0vtyg5557GA2dkdxtxpMoE0oM=", 1),
     -- Mot de passe : Password#2
     ("Default2", "default2@domain.com", "HM6qYGlj7hDQY0th-1dF8V45tUHZ5r0hoZ33f9jzINA=",
         "TzSfHbNruIckCsHO2OW52W9-TXarHderYhLWdXfWbn4=", 1);
@@ -24,6 +24,7 @@ INSERT INTO
 VALUES
     ("Problème d'affichage", 0, 1, "2021-12-20"),
     ("Plus d'encre", 0, 2, "2021-12-11"),
+    ("Plus de papier", 0, 2, "2021-12-11"),
     ("Ne fonctionne plus", 0, 3, "2021-11-24"),
     ("L'écran ne s'allume plus", 0, 4, "2021-12-05"),
     ("La toile ne descend plus", 0, 5, "2021-12-07");
